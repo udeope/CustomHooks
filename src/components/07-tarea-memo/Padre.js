8a2b7497ab481b5251ba55ca9723201494a81ab7
Eduard <edusv10@gmail.com>
2021-08-05 20:38:14 +0200

    Tarea memo useCallback

---
+++ src/components/07-tarea-memo/Padre.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Hijo } from './Hijo'
+import { useState, useCallback } from 'react';
+
+import '../02-useEffect/effects.css';
+
+export const Padre = () => {
+
+    const numeros = [2,4,6,8,10];
+    const [valor, setValor] = useState(0);
+
+    // const incrementar = ( num ) => {
+    //     setValor( valor + num )
+    // }
+
+    const incrementar = useCallback(
+      (num) => {
+        setValor((c) => c + num); //El valor anterior mas el nuevo numero
+      },
+      [setValor]
+    );
+
+
+    return (
+        <div>
+            <h1>Padre</h1>
+            <p> Total: { valor } </p>
+
+            <hr />
+
+            {
+                numeros.map( n => (
+                    <Hijo 
+                        key={ n }
+                        numero={ n }
+                        incrementar={ incrementar }
+                    />
+                ))
+            }
+            {/* <Hijo /> */}
+        </div>
+    )
+}
